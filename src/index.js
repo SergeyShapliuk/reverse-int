25356fef524f85427460944ff48dc903ccab6e96
SergeyShapliuk <sergeshapluk@gmail.com>
2022-07-02 11:00:58 +0300

    feat: add reverse number functional(index.js)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let reversNum=""
+    let number=n.toString().split("")
+    for (let i = 1;i<=number.length; i++) {
+        reversNum+=number[number.length-i]
+    }
+    return parseInt(reversNum)
 }
